f03911ddd007c2f821f3964265dc705c44d14825
std <std@SFC-LAB201-PC15>
2023-05-04 19:06:16 +0500

    Add project files.

---
+++ CoreShoppingCart/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreShoppingCart.Models
+{
+    [Table("TblCat")]
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CName { get; set; }
+        public bool CStatus { get; set; }
+    }
+}
